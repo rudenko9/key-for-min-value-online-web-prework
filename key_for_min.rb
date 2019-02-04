7722e5d22eb083c9b8e331d53476edead065ede8
Nataliya R <n.rudenko@gmail.com>
2019-02-04 21:35:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,8 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key,value| 
-   puts k 
-   if  v == name_hash.values.min
+   puts key
+   if  value == name_hash.values.min
   end
 end
 key
